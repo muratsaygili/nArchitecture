ae488fa4760f79876c02db01ee6ed87d85927d10
Murat Saygılı <murat@jbi.com.tr>
2022-09-04 19:16:29 +0300

    profiles

--- src/projects/Kodlama.io.Devs/WebAPI/Program.cs
+++ src/projects/Kodlama.io.Devs/WebAPI/Program.cs
@@ -20,11 +20,11 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 if (app.Environment.IsProduction())
     app.ConfigureCustomExceptionMiddleware();
